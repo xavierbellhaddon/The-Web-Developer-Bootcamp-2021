3c992014c66dd3ac39e5bc2e045e6768f2323f75
Austin Bell <snarkeet@gmail.com>
2021-11-07 01:13:02 -0500

    Else

--- JavaScript/Conditionals/app.js
+++ JavaScript/Conditionals/app.js
@@ -2,25 +2,25 @@
 
 // if (random < 0.5) {
 //     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-//     console.log(random)
+// } else {
+//     console.log("YOUR NUMBER IS GREATER THAN (OR EQUAL TO) 0.5");
 // }
 
-// if (random >= 0.5) {
-//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-//     console.log(random)
-// }
+// console.log(random);
 
-// const dayOfWeek =  "Friday";
+// const dayOfWeek =  prompt("ENTER A DAY").toLowerCase();
 
-// if (dayOfWeek === "Monday") {
+// if (dayOfWeek === "monday") {
 //     console.log("UGH I HATE MONDAYS!")
-// } else if (dayOfWeek === "Saturday") {
+// } else if (dayOfWeek === "saturday") {
 //     console.log("YAY I LOVE SATURDAYS!")
-// } else if (dayOfWeek === "Friday") {
+// } else if (dayOfWeek === "friday") {
 //     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
+// } else {
+//     console.log("MEH.");
 // }
 
-const age = 8;
+const age = 89;
 
 if (age < 5) {
     console.log("You are a baby. You get in for free!");
@@ -28,4 +28,6 @@ if (age < 5) {
     console.log("You are child. You pay $10.");
 } else if (age < 65) {
     console.log("You are an adult. You pay $20.");
+} else {
+    console.log("You are a senior. You pay $10.");
 }
