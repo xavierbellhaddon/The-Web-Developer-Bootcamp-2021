44fcb201573e1f55549d952f182af95ac40bd6d9
Austin Bell <snarkeet@gmail.com>
2021-11-07 01:58:58 -0500

    Logical OR

--- Logical Operators/app.js
+++ Logical Operators/app.js
@@ -1,7 +1,19 @@
-const password = prompt("Enter your password");
+// const password = prompt("Enter your password");
 
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-    console.log("VALID PASSWORD!");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//     console.log("VALID PASSWORD!");
+// } else {
+//     console.log("INCORRECT FORMAT!");
+// }
+
+const age = 50;
+
+if ((age < 5 && age >= 0) || age >= 65) {
+    console.log("FREE");
+} else if (age >=5 && age < 10) {
+    console.log("$10");
+} else if (age >=10 && age < 65) {
+    console.log("$20");
 } else {
-    console.log("INCORRECT FORMAT!");
+    console.log("INVALID AGE!")
 }
