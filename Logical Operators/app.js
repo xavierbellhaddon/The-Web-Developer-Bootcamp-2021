a49a0f5e3fca1e6bf07aba8a4a4ff8c518ebf1b2
Austin Bell <snarkeet@gmail.com>
2021-11-07 02:07:43 -0500

    Logical NOT

--- Logical Operators/app.js
+++ Logical Operators/app.js
@@ -6,14 +6,26 @@
 //     console.log("INCORRECT FORMAT!");
 // }
 
-const age = 50;
+// const age = 50;
 
-if ((age < 5 && age >= 0) || age >= 65) {
-    console.log("FREE");
-} else if (age >=5 && age < 10) {
-    console.log("$10");
-} else if (age >=10 && age < 65) {
-    console.log("$20");
-} else {
-    console.log("INVALID AGE!")
+// if ((age < 5 && age >= 0) || age >= 65) {
+//     console.log("FREE");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10");
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20");
+// } else {
+//     console.log("INVALID AGE!")
+// }
+
+// let firstName = prompt("Enter your first name");
+
+// if (!firstName) {
+//     prompt("Try again");
+// }
+
+const age = 8;
+
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log("You are not a baby or a senior!")
 }
