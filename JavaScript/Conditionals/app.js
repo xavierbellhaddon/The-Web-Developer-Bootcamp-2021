b60d4191b1e8ece0aaab6ee36931a0411dbf968b
Austin Bell <snarkeet@gmail.com>
2021-11-07 01:02:18 -0500

    Else-If

--- JavaScript/Conditionals/app.js
+++ JavaScript/Conditionals/app.js
@@ -1,11 +1,31 @@
-let random = Math.random();
+// let random = Math.random();
 
-if (random < 0.5) {
-    console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-    console.log(random)
-}
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//     console.log(random)
+// }
 
-if (random >= 0.5) {
-    console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-    console.log(random)
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+//     console.log(random)
+// }
+
+// const dayOfWeek =  "Friday";
+
+// if (dayOfWeek === "Monday") {
+//     console.log("UGH I HATE MONDAYS!")
+// } else if (dayOfWeek === "Saturday") {
+//     console.log("YAY I LOVE SATURDAYS!")
+// } else if (dayOfWeek === "Friday") {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
+// }
+
+const age = 8;
+
+if (age < 5) {
+    console.log("You are a baby. You get in for free!");
+} else if (age < 18) {
+    console.log("You are child. You pay $10.");
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20.");
 }
