b7c76d86f41b3ae9f757e0053a571d9237a4e6e0
Austin Bell <snarkeet@gmail.com>
2021-11-07 02:19:49 -0500

    Switch

---
+++ Switch/app.js
@@ -0,0 +1,25 @@
+const day = 6;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+  case 7:
+      console.log("Weekend!")
+      break;
+  default:
+    console.log("I don't know that!");
+}
